26d9b10205dd2cb6d6a2685d54f35b4dbcb231c4
Andrea Tang <atang@spokeo.com>
2015-09-27 17:03:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaAdventures::Application.config.secret_token = 'c3b0f41f625e9403b099def6c08a064bc0e9e1b234a84f6f8822a646de33af35550348431d54d57117454746f771ab8b71fb28105a00cbe75017cc8da8df4c3e'
